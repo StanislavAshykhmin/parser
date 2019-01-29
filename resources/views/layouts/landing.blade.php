43a4ead933850b697e09832bf4930667844af777
Oleg Kudryk <olegkydrik23@gmail.com>
2019-01-29 11:53:13 +0200

    add link for icon(link on site awesomicon)

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -21,6 +21,8 @@ URI     : https://colorlib.com
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Acme|Dancing+Script|Indie+Flower|Lobster|Pacifico" rel="stylesheet">
+    <!-- Awesomicon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <title>@yield('title')</title>
     @stack('styles')
 </head>
